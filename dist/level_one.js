8d9b0b08a9b62d1f0d5fb7adfc888f6fb7fef6cc
kerokerogeorge <kerokerogeorge@yahoo.co.jp>
2021-06-25 18:26:31 +0900

    added all keys

--- dist/level_one.js
+++ dist/level_one.js
@@ -4,11 +4,41 @@ var KeyEvent;
     KeyEvent[KeyEvent["KeyUp"] = 1] = "KeyUp";
 })(KeyEvent || (KeyEvent = {}));
 var letter = [
+    { code: 'KeyQ', keyRus: 'й' },
+    { code: 'KeyW', keyRus: 'ц' },
+    { code: 'KeyE', keyRus: 'у' },
+    { code: 'KeyR', keyRus: 'к' },
+    { code: 'KeyT', keyRus: 'е' },
+    { code: 'KeyY', keyRus: 'н' },
+    { code: 'KeyU', keyRus: 'г' },
+    { code: 'KeyI', keyRus: 'ш' },
+    { code: 'KeyO', keyRus: 'щ' },
+    { code: 'KeyP', keyRus: 'з' },
+    { code: 'BracketLeft', keyRus: 'х' },
+    { code: 'BracketRight', keyRus: 'ъ' },
     { code: 'KeyA', keyRus: 'ф' },
     { code: 'KeyS', keyRus: 'ы' },
     { code: 'KeyD', keyRus: 'в' },
     { code: 'KeyF', keyRus: 'а' },
     { code: 'KeyG', keyRus: 'п' },
+    { code: 'KeyH', keyRus: 'р' },
+    { code: 'KeyJ', keyRus: 'о' },
+    { code: 'KeyK', keyRus: 'л' },
+    { code: 'KeyL', keyRus: 'д' },
+    { code: 'Semicolon', keyRus: 'ж' },
+    { code: 'Quote', keyRus: 'э' },
+    { code: 'Backslash', keyRus: 'ё' },
+    { code: 'KeyZ', keyRus: 'я' },
+    { code: 'KeyX', keyRus: 'ч' },
+    { code: 'KeyC', keyRus: 'с' },
+    { code: 'KeyV', keyRus: 'м' },
+    { code: 'KeyB', keyRus: 'и' },
+    { code: 'KeyN', keyRus: 'т' },
+    { code: 'KeyM', keyRus: 'ь' },
+    { code: 'Comma', keyRus: 'б' },
+    { code: 'Period', keyRus: 'ю' },
+    { code: 'Slash', keyRus: '/' },
+    { code: 'Space', keyRus: ' ' },
 ];
 var Keylist = /** @class */ (function () {
     function Keylist() {
@@ -49,14 +79,13 @@ function pushedMotion(pressedKey, eventType) {
     var indexOfRusKey = RusKeyArray.indexOf(pressedKey);
     if (eventType === KeyEvent.KeyDown) {
         keyboardClass[indexOfRusKey].classList.add("pushed");
-        console.log("add");
     }
     if (eventType === KeyEvent.KeyUp) {
         keyboardClass[indexOfRusKey].classList.remove("pushed");
-        console.log("remove");
     }
 }
 window.addEventListener('keydown', function (pressedCode) {
+    console.log(pressedCode);
     pushedMotion(returnCorrespondentLetter(pressedCode), KeyEvent.KeyDown);
 });
 window.addEventListener('keyup', function (pressedCode) {
--- dist/level_one.ts
+++ dist/level_one.ts
@@ -1,11 +1,42 @@
 enum KeyEvent { KeyDown, KeyUp }
 
-let letter: { [key: string]: string }[] = [
+const letter: { [key: string]: string }[] = [
+    { code: 'KeyQ', keyRus: 'й' },
+    { code: 'KeyW', keyRus: 'ц' },
+    { code: 'KeyE', keyRus: 'у' },
+    { code: 'KeyR', keyRus: 'к' },
+    { code: 'KeyT', keyRus: 'е' },
+    { code: 'KeyY', keyRus: 'н' },
+    { code: 'KeyU', keyRus: 'г' },
+    { code: 'KeyI', keyRus: 'ш' },
+    { code: 'KeyO', keyRus: 'щ' },
+    { code: 'KeyP', keyRus: 'з' },
+    { code: 'BracketLeft', keyRus: 'х' },
+    { code: 'BracketRight', keyRus: 'ъ' },
     { code: 'KeyA', keyRus: 'ф' },
     { code: 'KeyS', keyRus: 'ы' },
     { code: 'KeyD', keyRus: 'в' },
     { code: 'KeyF', keyRus: 'а' },
     { code: 'KeyG', keyRus: 'п' },
+    { code: 'KeyH', keyRus: 'р' },
+    { code: 'KeyJ', keyRus: 'о' },
+    { code: 'KeyK', keyRus: 'л' },
+    { code: 'KeyL', keyRus: 'д' },
+    { code: 'Semicolon', keyRus: 'ж' },
+    { code: 'Quote', keyRus: 'э' },
+    { code: 'Backslash', keyRus: 'ё' },
+    { code: 'KeyZ', keyRus: 'я' },
+    { code: 'KeyX', keyRus: 'ч' },
+    { code: 'KeyC', keyRus: 'с' },
+    { code: 'KeyV', keyRus: 'м' },
+    { code: 'KeyB', keyRus: 'и' },
+    { code: 'KeyN', keyRus: 'т' },
+    { code: 'KeyM', keyRus: 'ь' },
+    { code: 'Comma', keyRus: 'б' },
+    { code: 'Period', keyRus: 'ю' },
+    { code: 'Slash', keyRus: '/' },
+    { code: 'Space', keyRus: ' ' },
+
 ]
 class Keylist {
     topElement: HTMLDivElement;
@@ -47,17 +78,14 @@ function returnCorrespondentLetter(pressedCode){
     return letterInfo[0].keyRus
 }
 
-
 function pushedMotion(pressedKey: string, eventType: KeyEvent){
     let keyboardClass: HTMLCollection = document.getElementsByClassName('key');
     let indexOfRusKey: number = RusKeyArray.indexOf(pressedKey);
     if(eventType === KeyEvent.KeyDown){
         keyboardClass[indexOfRusKey].classList.add("pushed");
-        console.log("add");
     }
     if(eventType === KeyEvent.KeyUp){
         keyboardClass[indexOfRusKey].classList.remove("pushed");
-        console.log("remove");
     }
 }
 
